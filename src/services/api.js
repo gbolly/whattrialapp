504ed6c27acafd471b5d9cfa3f5767bfccdb77a9
gbolly <gbolahan.okerayi@andela.com>
2023-11-24 01:33:29 +0100

    revert

--- src/services/api.js
+++ src/services/api.js
@@ -17,6 +17,7 @@ export const request = async (url, action, data = null) => {
       url,
       data,
       headers: headers,
+      'withCredentials': true
     });
     return response;
   } catch (error) {
